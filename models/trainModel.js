876e46b0c22eea58bd697fd61e115cb153f80f66
priyanshuss890 <103364248+priyanshuss890@users.noreply.github.com>
2025-02-13 01:46:07 +0530

    Update trainModel.js

--- models/trainModel.js
+++ models/trainModel.js
@@ -1,19 +1,10 @@
-const pool = require('../config/db');
-
-const addTrain = async (name, source, destination, totalSeats) => {
-    const [result] = await pool.query(
-        'INSERT INTO trains (name, source, destination, total_seats, available_seats) VALUES (?, ?, ?, ?, ?)',
-        [name, source, destination, totalSeats, totalSeats]
-    );
-    return result.insertId;
+const getTrainById = async (trainId) => {
+    const [rows] = await pool.query('SELECT * FROM trains WHERE id = ?', [trainId]);
+    return rows[0];
 };
 
-const getTrainsBetweenStations = async (source, destination) => {
-    const [rows] = await pool.query(
-        'SELECT * FROM trains WHERE source = ? AND destination = ?',
-        [source, destination]
-    );
-    return rows;
+module.exports = { 
+    addTrain, 
+    getTrainsBetweenStations, 
+    getTrainById 
 };
-
-module.exports = { addTrain, getTrainsBetweenStations };
